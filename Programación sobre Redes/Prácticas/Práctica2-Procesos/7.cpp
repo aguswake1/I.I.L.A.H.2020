9360d5dd7077e1ed1852c7ce9f997c97166156fc
aguswake1 <aguswake4@gmail.com>
2020-10-10 18:27:52 -0300

    subo todo prácticas 2- procesos

--- Programación sobre Redes/Prácticas/Práctica2-Procesos/7.cpp
+++ Programación sobre Redes/Prácticas/Práctica2-Procesos/7.cpp
@@ -8,12 +8,27 @@ int main()
 {
     int alturaArbol;
     cout << "Ingrese la altura del árbol binario: " << endl;
+    //si alturaArbol = 2 | cantProcesos = 2³-1
+    //si alturaArbol = 3 | cantProcesos = 2⁴-1
     cin >> alturaArbol;
 
     for (size_t i = 0; i < alturaArbol; i++)
     {
         int pid = fork();
-    }
 
+        if (pid > 0)
+        {
+            pid = fork();
+
+            if (pid == 0)
+            {
+                wait(NULL);
+                wait(NULL);
+                printf("PID %d | PPID %d\n", getpid(), getppid());
+                exit(0);
+            }
+        }
+    }
+    printf("PID %d | PPID %d\n", getpid(), getppid());
     return 0;
 }
