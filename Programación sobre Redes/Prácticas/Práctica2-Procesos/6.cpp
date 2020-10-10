adfb7d7042eb7cc704b56f1acbb3f2cb8cd5513b
aguswake1 <aguswake4@gmail.com>
2020-10-09 21:25:18 -0300

    gng y continuo tp

--- Programación sobre Redes/Prácticas/Práctica2-Procesos/6.cpp
+++ Programación sobre Redes/Prácticas/Práctica2-Procesos/6.cpp
@@ -3,28 +3,27 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <cstdlib>
+using namespace std;
 
-int main() {
-
-	int numeroProcesos;
-	std::cin >> numeroProcesos;
+int main()
+{
+	int cantidadProcesos;
+	cout << "Ingrese la cantidad de procesos que desee: " << endl;
+	cin >> cantidadProcesos; // Capturamos entrada del usuario.
 	int pid = 0;
 
-	for (int i = 0; i < numeroProcesos; ++i)
+	for (size_t i = 0; i < cantidadProcesos; i++) // Ciclo que clona procesos menos del último.
 	{
-
 		if (pid == 0)
 		{
-			std::cout << "Soy " << getpid() << " Hijo de " << getppid() << " y soy de color " << (i % 2 == 0 ? "negro" : "rojo") << std::endl;
+			// Hacemos que diga quien es, de quien es hijo y su color.
+			cout << "Soy " << getpid() << ", mi viejo es " << getppid() << " y soy de color " << (i % 2 == 0 ? "Negro" : "Rojo") << endl;
 			pid = fork();
-
-		} else {
-			wait(NULL);
-
 		}
-
-
+		else
+		{
+			wait(NULL);
+		}
 	}
 	return 0;
 }
-
--- Programación sobre Redes/Prácticas/Práctica2-Procesos/7.cpp
+++ Programación sobre Redes/Prácticas/Práctica2-Procesos/7.cpp
@@ -2,4 +2,18 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int alturaArbol;
+    cout << "Ingrese la altura del árbol binario: " << endl;
+    cin >> alturaArbol;
+
+    for (size_t i = 0; i < alturaArbol; i++)
+    {
+        int pid = fork();
+    }
+
+    return 0;
+}
