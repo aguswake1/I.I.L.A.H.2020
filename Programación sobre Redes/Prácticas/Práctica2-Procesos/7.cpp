4da50e8379eec28e45682d34ec8e563e22d49f30
aguswake1 <aguswake4@gmail.com>
2020-10-10 21:24:17 -0300

    variante del 7

--- Programación sobre Redes/Prácticas/Práctica2-Procesos/7.cpp
+++ Programación sobre Redes/Prácticas/Práctica2-Procesos/7.cpp
@@ -4,7 +4,8 @@
 #include <sys/wait.h>
 using namespace std;
 
-int main()
+// Propuesta original del enunciado: arbol binario (2 procesos x hijo) de altuna n
+/*int main()
 {
     int alturaArbol;
     cout << "Ingrese la altura del árbol binario: " << endl;
@@ -20,13 +21,52 @@ int main()
         {
             pid = fork();
 
-            if (pid == 0)
+            if (pid > 0)
             {
                 wait(NULL);
                 wait(NULL);
                 printf("PID %d | PPID %d\n", getpid(), getppid());
                 exit(0);
             }
+        }
+    }
+    printf("PID %d | PPID %d\n", getpid(), getppid());
+    return 0;
+}*/
+
+// variante: arbol altura n que genere m cantidad de procesos
+int main()
+{
+    int alturaArbol, cantProcesos;
+    do
+    {
+        cout << "Ingrese la altura del árbol binario:\n";
+        cin >> alturaArbol;
+        cout << "Ingrese la cantidad de procesos que generaran los procesos del árbol:\n";
+        cin >> cantProcesos;
+    } while (alturaArbol < 1 && cantProcesos < 1);
+
+    for (size_t i = 0; i < alturaArbol; i++)
+    {
+        int pid = 1;
+        // for de creacion
+        for (size_t j = 0; j < cantProcesos; j++)
+        {
+            if (pid > 0)
+            {
+                pid = fork();
+            }
+        }
+        //for de espera
+        if (pid > 0)
+        {
+            for (size_t j = 0; j < cantProcesos; j++)
+            {
+
+                wait(NULL);
+            }
+            printf("PID %d | PPID %d\n", getpid(), getppid());
+            exit(0);
         }
     }
     printf("PID %d | PPID %d\n", getpid(), getppid());
